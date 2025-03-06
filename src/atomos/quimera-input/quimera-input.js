6a90a44135e108147fe1d2943bd98cc2dd9fded9
xavikortes <javiercortesveliz@gmail.com>
2025-03-06 13:28:22 +0100

    fix. renderizando desde template strings externos y pasando children

--- src/atomos/quimera-input/quimera-input.js
+++ src/atomos/quimera-input/quimera-input.js
@@ -1,3 +1,137 @@
+const template = ({
+  label,
+  placeholder,
+  nombre,
+  valor,
+  deshabilitado,
+  "texto-validacion": textoValidacion,
+  contenido,
+}) => `
+<style>
+  quimera-input {
+    label {
+      display: flex;
+      flex-direction: column;
+      color: var(--color-texto-secundario);
+
+      .etiqueta {
+        margin-bottom: var(--espaciado-xxs);
+        font-size: var(--tamaño-letra-m);
+
+        .etiqueta-opcional {
+          color: var(--color-texto-deshabilitado);
+          font-size: var(--tamaño-letra-xs);
+          display: none;
+        }
+      }
+
+      input {
+        --color-borde: var(--color-texto-secundario);
+
+        outline: 0;
+        color: var(--color-texto-primario);
+        border: var(--tamaño-borde-s) solid var(--color-borde);
+        border-radius: var(--redondeo-borde-s);
+        padding: var(--espaciado-m);
+        font-size: var(--tamaño-letra-m);
+        background-color: transparent;
+
+        &:focus {
+          box-shadow: inset 0 0 0 var(--tamaño-borde-m) var(--color-borde);
+        }
+
+        &::placeholder {
+          color: var(--color-texto-deshabilitado);
+        }
+      }
+
+      .texto-validacion {
+        margin-top: var(--espaciado-xxs);
+        color: var(--color-texto-deshabilitado);
+        font-size: var(--tamaño-letra-s);
+
+        &::before {
+          content: "";
+          margin-right: var(--espaciado-xxs);
+        }
+      }
+    }
+
+    &[erroneo] {
+      input {
+        --color-borde: var(--color-error);
+      }
+
+      & .texto-validacion {
+        color: var(--color-error);
+
+        &::before {
+          content: "❌";
+        }
+      }
+    }
+
+    &[advertido] {
+      input {
+        --color-borde: var(--color-advertencia);
+      }
+
+      & .texto-validacion {
+        color: var(--color-advertencia);
+
+        &::before {
+          content: "⚠️";
+        }
+      }
+    }
+
+    &[valido] {
+      input {
+        --color-borde: var(--color-exito);
+      }
+
+      & .texto-validacion {
+        color: var(--color-exito);
+
+        &::before {
+          content: "✅";
+        }
+      }
+    }
+
+    &[opcional] label .etiqueta .etiqueta-opcional {
+      display: inline;
+    }
+
+    &[deshabilitado] {
+      span {
+        color: var(--color-texto-deshabilitado);
+      }
+
+      input {
+        background-color: var(--color-deshabilitado);
+        color: var(--negro);
+        border-color: var(--negro);
+      }
+
+      cursor: inherit;
+      pointer-events: none;
+    }
+  }
+</style>
+<label>
+    <span class="etiqueta">
+        ${label}
+        <span class="etiqueta-opcional">(opcional)</span>
+    </span>
+    <input type="text"
+      name="${nombre}"
+      placeholder="${placeholder ?? ""}"
+      value="${valor ?? ""}"
+      ${deshabilitado !== undefined ? "disabled" : ""} />
+    <span class="texto-validacion">${textoValidacion ?? ""}</span>
+</label>`;
+
 customElements.get("quimera-input") ||
   customElements.define(
     "quimera-input",
@@ -11,153 +145,31 @@ customElements.get("quimera-input") ||
         "deshabilitado",
       ];
 
+      rendered = false;
+
       connectedCallback() {
+        this.contenido = this.innerHTML;
         this.render();
+        this.rendered = true;
       }
 
       attributeChangedCallback(attr, viejo, nuevo) {
+        if (!this.rendered) return;
         if (viejo === nuevo) return;
 
         this.render();
       }
 
+      disconnectedCallback() {
+        this.rendered = false;
+      }
+
       render() {
-        const {
-          nombre,
-          label,
-          placeholder,
-          valor,
-          deshabilitado,
-          "texto-validacion": textoValidacion,
-        } = Object.fromEntries(
+        const attrs = Object.fromEntries(
           [...this.attributes].map((a) => [a.name, a.value])
         );
 
-        this.innerHTML = `
-        <style>
-          quimera-input {
-            label {
-              display: flex;
-              flex-direction: column;
-              color: var(--color-texto-secundario);
-
-              .etiqueta {
-                margin-bottom: var(--espaciado-xxs);
-                font-size: var(--tamaño-letra-m);
-
-                .etiqueta-opcional {
-                  color: var(--color-texto-deshabilitado);
-                  font-size: var(--tamaño-letra-xs);
-                  display: none;
-                }
-              }
-
-              input {
-                --color-borde: var(--color-texto-secundario);
-
-                outline: 0;
-                color: var(--color-texto-primario);
-                border: var(--tamaño-borde-s) solid var(--color-borde);
-                border-radius: var(--redondeo-borde-s);
-                padding: var(--espaciado-m);
-                font-size: var(--tamaño-letra-m);
-                background-color: transparent;
-
-                &:focus {
-                  box-shadow: inset 0 0 0 var(--tamaño-borde-m) var(--color-borde);
-                }
-
-                &::placeholder {
-                  color: var(--color-texto-deshabilitado);
-                }
-              }
-
-              .texto-validacion {
-                margin-top: var(--espaciado-xxs);
-                color: var(--color-texto-deshabilitado);
-                font-size: var(--tamaño-letra-s);
-
-                &::before {
-                  content: "";
-                  margin-right: var(--espaciado-xxs);
-                }
-              }
-            }
-
-            &[erroneo] {
-              input {
-                --color-borde: var(--color-error);
-              }
-
-              & .texto-validacion {
-                color: var(--color-error);
-
-                &::before {
-                  content: "❌";
-                }
-              }
-            }
-
-            &[advertido] {
-              input {
-                --color-borde: var(--color-advertencia);
-              }
-
-              & .texto-validacion {
-                color: var(--color-advertencia);
-
-                &::before {
-                  content: "⚠️";
-                }
-              }
-            }
-
-            &[valido] {
-              input {
-                --color-borde: var(--color-exito);
-              }
-
-              & .texto-validacion {
-                color: var(--color-exito);
-
-                &::before {
-                  content: "✅";
-                }
-              }
-            }
-
-            &[opcional] label .etiqueta .etiqueta-opcional {
-              display: inline;
-            }
-
-            &[deshabilitado] {
-              span {
-                color: var(--color-texto-deshabilitado);
-              }
-
-              input {
-                background-color: var(--color-deshabilitado);
-                color: var(--negro);
-                border-color: var(--negro);
-              }
-
-              cursor: inherit;
-              pointer-events: none;
-            }
-          }
-        </style>
-        <label>
-            <span class="etiqueta">
-                ${label}
-                <span class="etiqueta-opcional">(opcional)</span>
-            </span>
-            <input type="text"
-              name="${nombre}"
-              placeholder="${placeholder ?? ""}"
-              value="${valor ?? ""}"
-              ${deshabilitado !== undefined ? "disabled" : ""} />
-            <span class="texto-validacion">${textoValidacion ?? ""}</span>
-        </label>`;
+        this.innerHTML = template({ ...attrs, contenido: this.contenido });
       }
     }
   );
